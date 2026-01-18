5691670a42d4b1bd463facdc3aa452244c292933
deomiarn <deomicarnel@hotmail.com>
2026-01-18 11:11:01 +0100

    O(n + nlogn + mlogm)

---
+++ Arrays & Hashing/Valid Anagram.py
@@ -0,0 +1,39 @@
+# Valid Anagram
+
+# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+#
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+#
+# Example 1:
+#
+# Input: s = "racecar", t = "carrace"
+#
+# Output: true
+# Example 2:
+#
+# Input: s = "jar", t = "jam"
+#
+# Output: false
+# Constraints:
+#
+# s and t consist of lowercase English letters.
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_sort = sorted(s)
+        t_sort = sorted(t)
+
+        print(s_sort)
+        print(t_sort)
+
+        for i in range(len(s_sort)):
+            if s_sort[i] != t_sort[i]:
+                return False
+
+        return True
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isAnagram("racecar", "carrace")) # True
+    print(solution.isAnagram("jar", "jam")) # False
